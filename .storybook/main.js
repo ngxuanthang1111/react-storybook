73168f91aa162abf4641a48a9f42947098d75665
Nguyễn Xuân Thắng <ngxuanthang1111@gmail.com>
2020-10-17 17:02:15 +0700

    Learning React Storybook Complete

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-knobs',
+    '@storybook/addon-viewport',
+    '@storybook/addon-actions',
+    '@storybook/addon-a11y',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,23 @@
+import React from 'react';
+import { addDecorator, addParameters } from '@storybook/react';
+import { ThemeProvider, theme, CSSReset } from '@chakra-ui/core';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+addDecorator(story => (
+  <ThemeProvider theme={theme}>
+    <CSSReset />
+    {story()}
+  </ThemeProvider>
+));
+
+addDecorator(withKnobs);
+addDecorator(withA11y);
+
+addParameters({
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+});
